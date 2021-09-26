d7db779ead854f5805946a4a54c3f6725ac5b417
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-09-26 23:22:59 +0530

    push

--- holdings/js/script.js
+++ holdings/js/script.js
@@ -74,38 +74,6 @@ function fadeOut(){
 window.onload = fadeOut;
 // pre loader end
 
-// disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    alert ("Inspect action disabled 😉");
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-    alert ("Action not allowed");
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    alert ("Action not allowed");
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    alert ("Code Viewer disabled 😉");
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-    alert ("Page save Prohibited");
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)) {
-    alert ("Text selection Disabled");
-    return false;
- }
-}
-
-
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
