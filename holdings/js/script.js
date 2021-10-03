0b1c439bfe1653141b7d1f2094947dcdde135ba3
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-10-04 04:42:47 +0530

    API script

--- holdings/js/script.js
+++ holdings/js/script.js
@@ -38,6 +38,33 @@ $(document).ready(function(){
     })
 });
 
+// ==================================================================================================================
+
+//==========================================================================================================
+
+function randomQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#random_quote").html("<p id='random_quote'>" +
+          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+          ' (' + response.quoteAuthor + ')');
+      }
+  });
+}
+document.getElementById("get-activity").addEventListener("click", function(){
+  $(function() {
+    randomQuote();
+  });
+})
+
+$("button").click(function(){
+  randomQuote();
+});
+
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
